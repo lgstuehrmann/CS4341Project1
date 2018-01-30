040f514adc0c9a1776004bf5e840408187e4c16a
basnowden <basnowden@wpi.edu>
2018-01-30 14:26:23 -0500

    Moved readwrite functions into minimax.py

--- minimax.py
+++ minimax.py
@@ -5,6 +5,8 @@
 import numpy as np
 import referee
 import tester
+import os.path
+from threading import Thread
 # alpha_beta class containing alpha beta values 
 class alpha_beta:
 	def __init__(self, alpha, beta):
@@ -123,3 +125,61 @@ input: the state of the board
 output: the score of the board based on math-stuff
 """
 board_score():
+
+
+#File readwrite functions below
+def check_turn():
+	#True if our turn, false otherwise
+	return os.path.exists("Sno_Stu_Son.go")
+
+def check_end():
+	#True if game is over, false otherwise
+	return os.path.exists("end_game")
+
+def str_to_move(moveString):
+	moveList = moveString.split()
+	return Move(moveList[1], moveList[2], moveList[0])
+
+def move_to_str(move):
+	moveString = "{} {} {}".format(move.p, move.c, move.r)
+	return moveString
+
+def timeout():
+	with open("move_file", 'w') as f:
+		f.write("Sno_Stu_Son D 8")
+
+def make_move():
+	oppMove = Move('Z', 0, "groupname")
+	playerMove = Move('Z', 0, "Sno_Stu_Son")
+	if check_turn() is True:
+		if check_end() is False:
+			with open("move_file", 'r') as f:
+				move = f.readline()
+				if move is '':
+					#If the first line of the file is empty, then this is the first move of the game
+					#White stones, make a move
+					#playerMove = minimax(empty board_state)
+					print("Empty file")
+				else:
+					#If there is a move in the file, then this is not the first move of the game
+					#Black stones, send opponent's move to str_to_move
+					oppMove = str_to_move(move)
+			playerMove = minimax(next_board(oppMove))
+			moveString = move_to_str(playerMove)
+			with open("move_file", 'w') as f:
+				f.write(moveString)
+		else:
+			print("Game over")
+	else:
+		print("Not your turn")
+
+#make_move()
+
+# t1 = Thread(target=Timer, args=(9.9, timeout))
+# t2 = Thread(target=make_move, name=moveThread)
+
+# while check_turn() is False:
+# 	sleep(0.025)
+
+# t1.start()
+# t2.start()
